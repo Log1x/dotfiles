dc1e6312b02ecbefccc2eba329774bdf925a21a8
Brandon Nifong <brandon@tendency.me>
2018-02-25 18:41:42 -0500

    Add /usr/local/sbin to pathes to make Brew happy

--- home/.zsh/env.zsh
+++ home/.zsh/env.zsh
@@ -1,3 +1,3 @@
 # Export our paths
-export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
