6fe502e4255d3ab4d7dbde1232d8ce279f6537a3
Brandon <brandon@tendency.me>
2019-05-29 05:15:37 -0500

    Add Linuxbrew path for antigen

--- home/.zsh/antigen.zsh
+++ home/.zsh/antigen.zsh
@@ -1,4 +1,5 @@
 [[ -f /usr/local/share/antigen/antigen.zsh ]] && source /usr/local/share/antigen/antigen.zsh
+[[ -f /home/linuxbrew/.linuxbrew/share/antigen/antigen.zsh ]] && source /home/linuxbrew/.linuxbrew/share/antigen/antigen.zsh
 
 # Load the oh-my-zsh library.
 antigen use oh-my-zsh
